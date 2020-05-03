28d1af510ca48fe35c8f4fd2f971c61f8e4fd058
kokoriru <ogihara.yuka.b@gmail.com>
2020-05-03 11:36:41 +0900

    取得した props である allPostsData を受け取る

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
