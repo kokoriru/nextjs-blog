94d117b55316cab2cf3f4a316ff80fa89afb81b7
kokoriru <ogihara.yuka.b@gmail.com>
2020-05-04 21:00:09 +0900

    動的ページ作成

---
+++ pages/posts/[id].js
@@ -0,0 +1,17 @@
+import Layout from '../../components/layout'
+
+export default Post() {
+  return (
+    <Layout>
+      <Head>
+        <title>Post</title>
+      </Head>
+      <h1>Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
