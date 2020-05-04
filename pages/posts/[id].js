e2a38672124902f269ccc70d3c30f5bb6eeac053
kokoriru <ogihara.yuka.b@gmail.com>
2020-05-04 21:17:35 +0900

    先ほどの関数をインポート

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default Post() {
   return (
