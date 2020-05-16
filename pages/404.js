75db7aea10dbe83d7a089f297a51fa7620a88143
kokoriru <ogihara.yuka.b@gmail.com>
2020-05-16 17:23:45 +0900

    404 ページの内容修正

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <>
+      <Head>
+        <title>404エラー</title>
+      </Head>
+      <div>
+        <h1>404エラー</h1>
+        <h2>お探しのページが見つかりませんでした</h2>
+        <p>
+          ページが一時的にアクセスできない状況にあるか
+          移動もしくは削除された可能性があります。
+          下記のリンクよりご覧になりたいページをお探しください。
+        </p>
+      </div>
+      <Link href='/'><a>TOPページ</a></Link>
+    </>
+  )
 }
